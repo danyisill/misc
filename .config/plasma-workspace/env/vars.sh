1ae71e309f9996445b11ba2ef66ac960e3cf244d
Daniel <23189912+danyisill@users.noreply.github.com>
2022-04-01 08:50:37 +0300

    Update vars.sh

--- .config/plasma-workspace/env/vars.sh
+++ .config/plasma-workspace/env/vars.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 #export MOZ_DISABLE_RDD_SANDBOX=1
 #export GTK_USE_PORTAL=1
+export SSH_ASKPASS='/usr/bin/ksshaskpass'
+eval $(ssh-agent)
 if pgrep -f startplasma-wayland
 then
 	export MOZ_ENABLE_WAYLAND=1
 	export MOZ_USE_XINPUT2=1
 	export QT_QPA_PLATFORM=wayland
+	#export SDL_VIDEODRIVER=wayland
 fi
